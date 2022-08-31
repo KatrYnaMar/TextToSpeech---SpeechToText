dc75c99f0472edc9842641d2a3daa29379327cc8
Katrina Marasigan <111042646+KatrYnaMar@users.noreply.github.com>
2022-08-31 15:19:44 +0800

    Delete texttospeech.py

--- texttospeech.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3 # pyttsx3 is a text-to-speech conversion library in Python
-eng= pyttsx3.init()
-
-while True:
-    answer= input("Type Something: ")
-    eng.say(answer)
-    eng.runAndWait()
-
